e7ad81039d4cdfe422b887b4ff40db41cc767b22
Red Wei <hevilhuy2@gmail.com>
2022-07-22 22:23:43 +0700

    Added examples.

--- ErrorHandling/Program.cs
+++ ErrorHandling/Program.cs
@@ -10,9 +10,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<ExceptionRecorderService>();
 builder.Services.AddHttpClient<BlazorSchoolHttpClientWrapper>((sp, httpClient) => httpClient.BaseAddress = new(builder.HostEnvironment.BaseAddress));
 
-builder.Services.AddScoped<ExceptionRecorderService2>();
-builder.Services.AddHttpClient("Blazor School HttpClient with middleware", httpClient => httpClient.BaseAddress = new(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BlazorSchoolHttpClientMiddleware>();
-builder.Services.AddTransient<BlazorSchoolHttpClientMiddleware>();
-
 await builder.Build().RunAsync();
---
+++ ErrorHandling/Utils/BlazorSchoolErrorBoundary.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace ErrorHandling.Utils;
+
+public class BlazorSchoolErrorBoundary : ErrorBoundary
+{
+    [Inject]
+    public ExceptionRecorderService ExceptionRecorderService { get; set; } = default!;
+
+    public BlazorSchoolErrorBoundary()
+    {
+        MaximumErrorCount = 2;
+    }
+
+    protected override Task OnErrorAsync(Exception exception)
+    {
+        ExceptionRecorderService.Exceptions.Add(exception);
+
+        return Task.CompletedTask;
+    }
+}
